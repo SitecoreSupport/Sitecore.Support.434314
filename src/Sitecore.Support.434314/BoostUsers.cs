ca95b25cf0526a99f1fef08f795f6c1968b73914
AlexanderYaremenko <ay@sitecore.net>
2017-03-15 15:40:49 +0200

    Fix the issue with Boost Users functionality

---
+++ src/Sitecore.Support.434314/BoostUsers.cs
@@ -0,0 +1,21 @@
+﻿    namespace Sitecore.Client.LicenseOptions.Controllers
+    {
+    using Security.Accounts;
+    using Sitecore;
+    using Sitecore.Text;
+    using Sitecore.Web;
+    using Sitecore.Web.Authentication;
+    using System.Web.Mvc;
+
+    public class BoostUsersInvalidatorController : Controller
+        {
+            [HttpGet]
+            public ActionResult Invalidate()
+            {
+                Boost.Invalidate();
+                UrlString str = new UrlString(WebUtil.GetFullUrl(new StartUrlManager().GetStartUrl(Context.User)));
+                base.Response.Redirect(str.ToString());
+                return base.Content("Boost cache is cleaned up");
+            }
+        }
+    }
---
+++ src/Sitecore.Support.434314/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.434314")]
+[assembly: AssemblyProduct("Sitecore.Support.434314")]
+[assembly: ComVisible(false)]
